9a722c6411f7c13935af3d79aeab0e5a400c36cf
Anna <annarsve@gmail.com>
2023-06-24 17:50:58 +0500

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import CardPage from "./pages/card-page/Card-page";
 import OddPage from "./pages/odd-page/Odd-page";
 import UserPage from "./pages/user-page/User-page";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import RegisterPage from "./pages/register-page/Register-page";
+import LoginPage from "./pages/login-page/Login-page";
 
 function App() {
 
+    const isAuth = false;
   return (
       <div className="App">
           <header className="container">
@@ -21,16 +24,48 @@ function App() {
                       </button>
                       <div className="collapse navbar-collapse" id="navbarNav">
                           <ul className="navbar-nav">
-                              <li className="nav-item">
-                                  <Link className="nav-link" to="/">CardPage</Link>
-                                  {/*<a className="nav-link active" aria-current="page" href="#">Home</a>*/}
-                              </li>
+                              { isAuth ?
+                                  <li className="nav-item">
+                                      <Link className="nav-link" to="/">CardPage</Link>
+                                      {/*<a className="nav-link active" aria-current="page" href="#">Home</a>*/}
+                                  </li> : <li> </li>
+                              }
                               <li className="nav-item">
                                   <Link className="nav-link" to="/odd">oddPage</Link>
                                   {/*<a className="nav-link" href="#">Features</a>*/}
                               </li>
                               <li className="nav-item">
-                                  <Link className="nav-link" to="/user">userPage</Link>
+                                  { isAuth ?
+                                      <li className="nav-item">
+                                          <Link className="nav-link" to="/user">userPage</Link>
+                                          {/*<a className="nav-link active" aria-current="page" href="#">Home</a>*/}
+                                      </li> : <li> </li>
+                                  }
+                                  {/*<a className="nav-link" href="#">Pricing</a>*/}
+                              </li>
+                              <li className="nav-item">
+                                  { isAuth ?
+                                     <li className="nav-link">
+                                         <button>
+                                             logout
+                                         </button>
+                                     </li> :
+                                      <li className="nav-item">
+                                          <Link className="nav-link" to="/login">Login</Link>
+                                          {/*<a className="nav-link active" aria-current="page" href="#">Home</a>*/}
+                                      </li>
+                                  }
+                                  {/*<a className="nav-link" href="#">Pricing</a>*/}
+                              </li>
+                              <li className="nav-item">
+                                  { isAuth ?
+                                      <li >
+                                      </li> :
+                                      <li className="nav-item">
+                                          <Link className="nav-link" to="/register">Register</Link>
+                                          {/*<a className="nav-link active" aria-current="page" href="#">Home</a>*/}
+                                      </li>
+                                  }
                                   {/*<a className="nav-link" href="#">Pricing</a>*/}
                               </li>
                           </ul>
@@ -42,6 +77,8 @@ function App() {
           <Route path="/" element={<CardPage />} />
           <Route path="/odd" element={<OddPage />} />
           <Route path="/user" element={<UserPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
         {/*<header className="App-header">*/}
         {/*  <img src={logo} className="App-logo" alt="logo" />*/}
---
+++ src/pages/login-page/Login-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useDispatch} from "react-redux";
+
+
+const LoginPage = () => {
+
+    return <div>
+      login
+    </div>
+};
+
+export default LoginPage;
